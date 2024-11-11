40cc8061541aba26c23842f87bba1ba32d82a246
Tim @ LEAP <a@b>
2024-11-11 12:25:15 +0000

    added several new features
    - improved way to deal with different hyperparameter combinations, order of hyperparameter settings for n_runs >= n_combinations is now always the same, this may have affected labeling for Wick & earlier
    - now storing tensorflow model
    - accommodated changing function arguments through bash, otherwise using default settings

--- train_and_predict.py
+++ train_and_predict.py
@@ -4,24 +4,26 @@ import xarray as xr
 import fnmatch
 import numpy as np
 import os
+import sys
 import keras
 from surgeNN.io import load_predictand,load_predictors
-from surgeNN.preprocessing import get_denseloss_weights,split_predictand_and_predictors_chronological,split_predictand_and_predictors_stratified_years
+from surgeNN.denseLoss import get_denseloss_weights #if starting with a clean environment, first, in terminal, do->'mamba install kdepy'
+from surgeNN.preprocessing import split_predictand_and_predictors_chronological,split_predictand_and_predictors_with_stratified_years
 from surgeNN.preprocessing import generate_batched_windowed_filtered_tf_input, generate_windowed_filtered_np_input, deseasonalize_da, deseasonalize_df_var
-from surgeNN.evaluation import compute_statistics_on_output_ds,rmse
+from surgeNN.evaluation import add_error_metrics_to_prediction_ds,rmse
 from surgeNN.preprocessing import stack_predictors_for_lstm, stack_predictors_for_convlstm, normalize_predictand_splits, normalize_predictor_splits,standardize_predictand_splits, standardize_predictor_splits
 from surgeNN.models import build_LSTM_stacked, build_ConvLSTM2D_with_channels
 from surgeNN.losses import gevl,exp_negexp_mse,obs_squared_weighted_mse, obs_weighted_mse
 from sklearn.metrics import confusion_matrix
 from tqdm import tqdm
-from target_relevance import TargetRelevance #if starting with a clean environment, first, in terminal, do->'mamba install kdepy'
+import itertools
 
 import gc #callback to clean up garbage after each epoch, not sure if needed (usage: callbacks = [GC_Callback()])
 class GC_Callback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         gc.collect()
         
-def train_and_predict(loss_function,loss_function_,possible_params,n_combinations,
+def train_and_predict(loss_function,loss_function_,possible_params,
                       split_fractions,split_method,split_start_month,split_seed,
                       n_runs,n_iterations,n_epochs,patience,
                      input_dir,output_dir,
@@ -48,7 +50,7 @@ def train_and_predict(loss_function,loss_function_,possible_params,n_combination
             predictors[var] = deseasonalize_da(predictors[var]) #remove mean seasonal cycle
 
         #load & process predictands
-        predictand = load_predictand('/home/jovyan/test_surge_models/input/t_tide_'+str(temp_freq)+'h_anoms_deseasoned_predictands',tg) #open predictand csv
+        predictand = load_predictand('/home/jovyan/test_surge_models/input/t_tide_'+str(temp_freq)+'h_hourly_deseasoned_predictands',tg) #open predictand csv
         predictand = predictand[(predictand['date']>=predictors.time.isel(time=0).values) & (predictand['date']<=predictors.time.isel(time=-1).values)]  # only use predictands in the period for which we also have predictor values, including at preceding timesteps
         predictand = deseasonalize_df_var(predictand,'surge','date') #remove mean seasonal cycle
 
@@ -56,27 +58,25 @@ def train_and_predict(loss_function,loss_function_,possible_params,n_combination
         predictand = predictand.reset_index()[['surge','date','lon','lat']]
         #predictand['surge'] = predictand['surge'].rolling(window=int(12/temp_freq+1),min_periods=int(12/temp_freq+1),center=True).mean() #crude way to filter out peaks due to uncorrected tides (Tiggeloven et al., 2021)
 
+        #configure hyperparameter settings
+        possible_settings = list(itertools.product(*possible_params))
+        n_settings = len(possible_settings)
+        
+        if n_runs<n_settings:
+            selected_settings = np.random.choice(possible_settings, n_runs, replace=False)
+        else:
+            selected_settings = possible_settings
+        
         #train & evaluate models n_iterations * n_runs times:
         for it in np.arange(n_iterations):
             tg_datasets = [] #list to store output
-            all_params = [] #list of hyperparameters
-            for i in tqdm(np.arange(n_runs if n_runs < n_combinations else n_combinations)): #loop over hyperparameter combinations
-
-                #randomly select unique combinations of hyperparameters
-                k=True 
-                while k == True:
-                    these_params = [np.random.choice(k) for k in possible_params] 
-                    if these_params in all_params:
-                        k = True
-                    else:
-                        k = False
-                        all_params.append(these_params) #keep track of hyperparameter combinations
-
-                this_batch_size,this_n_steps,this_n_convlstm,this_n_convlstm_units,this_n_dense,this_n_dense_units,this_dropout,this_lr,this_l2,this_dl_alpha = these_params
+            
+            for i,these_settings in enumerate(selected_settings): #loop over hyperparameter combinations
+                this_batch_size,this_n_steps,this_n_convlstm,this_n_convlstm_units,this_n_dense,this_n_dense_units,this_dropout,this_lr,this_l2,this_dl_alpha =these_settings
 
                 #generate splits
                 #idx_train,idx_val,idx_test,x_train,x_val,x_test,y_train,y_val,y_test = split_predictand_and_predictors_chronological(predictand,predictors,split_fractions,this_n_steps)
-                idx_train,idx_val,idx_test,x_train,x_val,x_test,y_train,y_val,y_test = split_predictand_and_predictors_stratified_years(predictand,predictors,
+                idx_train,idx_val,idx_test,x_train,x_val,x_test,y_train,y_val,y_test = split_predictand_and_predictors_with_stratified_years(predictand,predictors,
                                                                                                                                         split_fractions,this_n_steps,split_start_month,seed=split_seed,how=split_method)
 
                 #standardize the input based on the mean & sd of the train split
@@ -131,17 +131,17 @@ def train_and_predict(loss_function,loss_function_,possible_params,n_combination
                 yhat_test = model.predict(x_test,verbose=0).flatten()*y_train_sd + y_train_mean
 
                 #store results into xr dataset for current i-j
-                ds_train = xr.Dataset(data_vars=dict(o=(["time"], o_train),yhat=(["time"], yhat_train),hyperparameters=(['p'],these_params),),
+                ds_train = xr.Dataset(data_vars=dict(o=(["time"], o_train),yhat=(["time"], yhat_train),hyperparameters=(['p'],list(these_settings)),),
                 coords=dict(time=t_train,p=['batch_size', 'n_steps', 'n_convlstm', 'n_convlstm_units','n_dense', 'n_dense_units', 'dropout', 'lr', 'l2','dl_alpha'],),
-                attrs=dict(description="ConvLSTM - neural network prediction performance.",loss_function=loss_function),)
+                attrs=dict(description=architecture+" - neural network prediction performance.",loss_function=loss_function),)
 
-                ds_val = xr.Dataset(data_vars=dict(o=(["time"], o_val),yhat=(["time"], yhat_val),hyperparameters=(['p'],these_params),),
+                ds_val = xr.Dataset(data_vars=dict(o=(["time"], o_val),yhat=(["time"], yhat_val),hyperparameters=(['p'],list(these_settings)),),
                 coords=dict(time=t_val,p=['batch_size', 'n_steps', 'n_convlstm', 'n_convlstm_units','n_dense', 'n_dense_units', 'dropout', 'lr', 'l2','dl_alpha'],),
-                attrs=dict(description="ConvLSTM - neural network prediction performance.",loss_function=loss_function),)
+                attrs=dict(description=architecture+" - neural network prediction performance.",loss_function=loss_function),)
 
-                ds_test = xr.Dataset(data_vars=dict(o=(["time"], o_test),yhat=(["time"], yhat_test),hyperparameters=(['p'],these_params),),
+                ds_test = xr.Dataset(data_vars=dict(o=(["time"], o_test),yhat=(["time"], yhat_test),hyperparameters=(['p'],list(these_settings)),),
                 coords=dict(time=t_test,p=['batch_size', 'n_steps', 'n_convlstm', 'n_convlstm_units','n_dense', 'n_dense_units', 'dropout', 'lr', 'l2','dl_alpha'],),
-                attrs=dict(description="ConvLSTM - neural network prediction performance.",loss_function=loss_function),)
+                attrs=dict(description=architecture+" - neural network prediction performance.",loss_function=loss_function),)
 
                 ds_i = xr.concat((ds_train,ds_val,ds_test),dim='split',coords='different') #concatenate results for each split
                 ds_i = ds_i.assign_coords(split = ['train','val','test'])
@@ -156,14 +156,21 @@ def train_and_predict(loss_function,loss_function_,possible_params,n_combination
                 ds_i['val_loss'] = (['e'],val_loss)
 
                 tg_datasets.append(ds_i) #append output of current iteration to list of all outputs
+                
+                if store_model:
+                    my_path = os.path.join(output_dir,'keras_models',architecture)
+                    my_fn = architecture+'_'+str(temp_freq)+'h_'+tg.replace('.csv','')+'_'+loss_function+'_hp1_i'+str(i)+'_it'
 
+                    model.save(os.path.join(my_path,
+                     my_fn+str(len(fnmatch.filter(os.listdir(my_path),my_fn+'*')))+'.keras'))
+                
                 del model, train_history, ds_i #, x_train, x_val, x_test
                 tf.keras.backend.clear_session()
                 gc.collect()
 
             #concatenate across runs & compute statistics
             out_ds = xr.concat(tg_datasets,dim='i',coords='different')
-            out_ds = compute_statistics_on_output_ds(out_ds,[.95,.98,.99,.995])
+            out_ds = add_error_metrics_to_prediction_ds(out_ds,[.95,.98,.99,.995])
 
             out_ds = out_ds.assign_coords(tg = np.array([tg]))
             out_ds = out_ds.assign_coords(lon = ('tg',np.array([predictand['lon'].values[0]])))
@@ -176,39 +183,62 @@ def train_and_predict(loss_function,loss_function_,possible_params,n_combination
             out_ds.attrs['loss_function'] = loss_function
             out_ds.attrs['split_fractions'] = split_fractions
             out_ds.attrs['split_method'] = split_method+'_'+str(split_start_month)+'_'+str(split_seed)
-
-            out_fn = architecture+'_'+str(temp_freq)+'h_'+tg.replace('.csv','')+'_'+loss_function+'_hp1_it'
-            out_ds.to_netcdf(os.path.join(output_dir,out_fn+str(len(fnmatch.filter(os.listdir(output_dir),out_fn+'*')))+'_testa.nc'),mode='w')
+            
+            my_path = os.path.join(output_dir,'performance',architecture)
+            my_fn = architecture+'_'+str(temp_freq)+'h_'+tg.replace('.csv','')+'_'+loss_function+'_hp1_it'
+            
+            out_ds.to_netcdf(os.path.join(my_path,my_fn+str(len(fnmatch.filter(os.listdir(my_path),my_fn+'*')))+'.nc'),mode='w')
     return out_ds
 
 if __name__ == "__main__":
+    '''
     tgs        = ['stavanger-svg-nor-nhs.csv','wick-wic-gbr-bodc.csv','esbjerg-esb-dnk-dmi.csv',
                   'immingham-imm-gbr-bodc.csv','den_helder-denhdr-nld-rws.csv', 'fishguard-fis-gbr-bodc.csv',  
-                  'brest-822a-fra-uhslc.csv', 'vigo-vigo-esp-ieo.csv',  'alicante_i_outer_harbour-alio-esp-da_mm.csv'] #tide gauges to process
-    tgs = ['alicante_i_outer_harbour-alio-esp-da_mm.csv']
+                  'brest-822a-fra-uhslc.csv', 'vigo-vigo-esp-ieo.csv',  'alicante_i_outer_harbour-alio-esp-da_mm.csv'] #all tide gauges to process
+    '''
+    # --- allow to set specific function arguments using commandline in "execute_train_and_predit.sh"
+    default_tgs = ['den_helder-denhdr-nld-rws.csv'] #set default values
+    default_architecture = 'lstm'
+    default_alpha = np.array([0,1,3,5]).astype('int')
+
+    arguments = sys.argv
+    if len(arguments)==4: #if specifying tgs, architecture & alpha values from commandline
+        tgs = [arguments[1]] #tg to process
+        architecture = arguments[2] #architecture to use
+        dl_alpha = eval(arguments[3]) #density-based weights tuning parameter
+    else:
+        tgs = default_tgs
+        architecture = default_architecture
+        dl_alpha = default_alpha
+    
+    print('Training & predicting for tide gauge: '+str(tgs))
+    print('Architecture: '+architecture+'; dl_alpha: ' + str(dl_alpha))
+    # --- 
+    
     temp_freq = 3 # [hours] temporal frequency to use
     n_cells   = 5 #n x n grid cells around tide gauge of predictor data to use
 
     var_names = ['msl','u10','v10','w'] #variables to use
 
     loss_function = 'mse' # default tensorflow loss function string or string of custom loss function of surgeNN.losses (e.g., 'gevl({gamma})')
-    architecture = 'lstm'
-
+    
     input_dir  = '/home/jovyan/test_surge_models/input/' #directory with predictor & predictand data
-    output_dir = os.path.join('/home/jovyan/test_surge_models/results/nn_tests/',architecture+'/') #where to store the results
+    output_dir = '/home/jovyan/test_surge_models/results/nn_tests/'#performance/',architecture+'/') #where to store the results
 
     n_runs = 24 #how many iterations with different hyperparameter combinations to run
     n_iterations = 1
     n_epochs = 100 #how many training epochs
     patience = 10 #early stopping patience
 
+    store_model = 1
+    
     split_fractions = [.6,.2,.2] #train, test, val
     split_method = '99pct'
     split_start_month = 7
     split_seed = 0
 
     #hyperparameters:
-    dl_alpha = np.array([0,1,3,5]).astype('int')
+    #dl_alpha = np.array([0,1,3,5]).astype('int')
     batch_size = np.array([128]).astype('int')
     n_steps = np.array([9]).astype('int') #at and 12h or 24h or 36h before (to-do: also use timesteps after?)
     n_convlstm = np.array([1]).astype('int')
@@ -221,10 +251,8 @@ if __name__ == "__main__":
     
     possible_params = [batch_size, n_steps, n_convlstm, n_convlstm_units,
                     n_dense, n_dense_units, dropout, lrs, l1s, dl_alpha]
-
-    n_combinations = np.prod(np.array([len(k) for k in possible_params])) #number of unique combinations possible
-
-    out_ds = train_and_predict(loss_function,loss_function_,possible_params,n_combinations,
+    
+    out_ds = train_and_predict(loss_function,loss_function,possible_params, #execute
                       split_fractions,split_method,split_start_month,split_seed,
                       n_runs,n_iterations,n_epochs,patience,
                      input_dir,output_dir,
