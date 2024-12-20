2884556b1b22ca7f4789145888ea4a8488d4734c
Tim @ LEAP <a@b>
2024-12-20 12:20:53 +0000

    changed handling of input region size

--- surgeNN/io.py
+++ surgeNN/io.py
@@ -21,16 +21,15 @@ def load_predictand(input_dir,tg): #open csv files with predictands for a tide g
     predictand['date'] = pd.to_datetime(predictand['date'])
     return predictand
 
-def load_predictors(input_dir,tg,n_cells): #open ERA5 wind and pressure predictors around a tide gauge
+def load_predictors(input_dir,tg): #open ERA5 wind and pressure predictors around a tide gauge
     '''
     input_dir:   directory where predictors are stored
     tg:          name of tide gauge to open predictors for
-    n_cells:     number of grid cells around tide gauge to use predictors at
     '''
     
     if input_dir.startswith('gs://'):
-        predictors = xr.open_dataset(os.path.join(input_dir,tg.replace('.csv','_era5Predictors_'+str(n_cells)+'x'+str(n_cells)+'.nc')),engine='zarr')
+        predictors = xr.open_dataset(os.path.join(input_dir,tg.replace('.csv','_era5Predictors_5x5.nc')),engine='zarr')
     else:
-        predictors = xr.open_dataset(os.path.join(input_dir,tg.replace('.csv','_era5Predictors_'+str(n_cells)+'x'+str(n_cells)+'.nc')))
+        predictors = xr.open_dataset(os.path.join(input_dir,tg.replace('.csv','_era5Predictors_5x5.nc')))
     predictors['w'] = np.sqrt(predictors['u10']**2+predictors['v10']**2) #compute wind speed from x/y components
     return predictors
