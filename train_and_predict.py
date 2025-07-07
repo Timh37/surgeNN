c4ce4d4da5255d52bbb968521ac46d55f39ab49b
Tim @ LEAP <a@b>
2025-07-07 13:34:18 +0000

    rewritten to use io and preprocessing classes

--- train_and_predict.py
+++ train_and_predict.py
@@ -6,8 +6,9 @@ import numpy as np
 import os
 import sys
 import keras
-from surgeNN.io import load_predictand,load_codec_as_predictand,load_predictors,train_predict_output_to_ds, setup_output_dirs
-from surgeNN.denseLoss import get_denseloss_weights #if starting with a clean environment, first, in terminal, do->'mamba install kdepy'
+from surgeNN import io, preprocessing
+from surgeNN.io import train_predict_output_to_ds, setup_output_dirs
+#from surgeNN.denseLoss import get_denseloss_weights #if starting with a clean environment, first, in terminal, do->'mamba install kdepy'
 from surgeNN.preprocessing import split_predictand_and_predictors_chronological,split_predictand_and_predictors_with_stratified_years
 from surgeNN.preprocessing import generate_batched_windowed_filtered_tf_input, generate_windowed_filtered_np_input, deseasonalize_da, deseasonalize_df_var
 from surgeNN.evaluation import add_error_metrics_to_prediction_ds,rmse
@@ -26,7 +27,7 @@ class GC_Callback(tf.keras.callbacks.Callback):
         
 def train_and_predict(model_architecture,loss_function,hyperparam_options,
                       split_fractions,strat_metric,strat_start_month,strat_seed,tgs,n_runs,n_iterations,n_epochs,patience,
-                      predictor_path,predictor_vars,predictor_degrees,predictand_source,predictand_path,temp_freq,output_dir,store_model):
+                      predictor_path,predictor_vars,predictor_degrees,predictand_path,temp_freq,output_dir,store_model):
     '''
     Execute model training and prediction and evaluation on test split. Uses a stratified splitting scheme.
     
@@ -47,7 +48,6 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
         predictor_path:      path to load predictor data from (must point to directories organized per temporal frequency) [str]
         predictor_vars:      predictor variables to use [list]
         predictor_degrees:   number of degrees of predictors around tide gauge to use [int/float]
-        predictand_source:   type of predictand data to use (currently implemented: 'gesla3' or 'codec') [str]
         predictand_path:     path to load predictand data from (must point to directories organized per temporal frequency) [str]
         temp_freq: temporal  frequency of input data to use [int]
         output_dir:          directory to store models and model performance to [str]
@@ -68,39 +68,22 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
         
     for tg in tqdm(tgs): #loop over TGs:
         
-        ### (1) Load & process predictors
-        predictors = load_predictors(os.path.join(predictor_path,str(temp_freq)+'hourly'),tg)
-        predictors = predictors.sel(time=slice('1979','2017')) #2017 because of end year GTSM simulations that are used as benchmark
+        ### (1) Load & prepare input data
+        n_cells = int(predictor_degrees * (4/1)) #determine how many grid cells around TG to use (era5 resolution = 0.25 degree)
         
-        #determine how many grid cells around TG to use
-        if predictor_degrees > 5:
-            print('Cannot use more grid cells than provided in the 5x5 input predictor files, so setting predictor_degrees to 5 instead of '+str(predictor_degrees))
-            predictor_degrees = 5
-        n_cells = int(predictor_degrees * 4) #era5 resolution = 0.25 degree
-
-        predictors = predictors.isel(lon_around_tg = np.arange(0+int((20-n_cells)/2),20-int((20-n_cells)/2)),
-                                     lat_around_tg = np.arange(0+int((20-n_cells)/2),20-int((20-n_cells)/2))) #standard is 20 by 20, reduce if n_cells<5
+        predictors = io.Predictor(predictor_path)
+        predictors.open_dataset(tg,predictor_vars,n_cells)
+        predictors.trim_years(1979,2017)
+        predictors.subtract_annual_means()
+        predictors.deseasonalize()
+ 
+        predictand = io.Predictand(predictand_path)
+        predictand.open_dataset(tg)
+        predictand.trim_dates(predictors.data.time.isel(time=0).values,predictors.data.time.isel(time=-1).values)
+        predictand.deseasonalize()
+        predictand.resample_fillna(str(temp_freq)+'h')
     
-        for var in predictor_vars: #remove annual means from each variable
-            predictors[var] = predictors[var].groupby(predictors.time.dt.year) - predictors[var].groupby(predictors.time.dt.year).mean('time') #remove annual means
-            predictors[var] = deseasonalize_da(predictors[var]) #remove mean seasonal cycle
-
-        ### (2) Load & process predictands
-        if predictand_source.lower() == 'gesla3':
-            predictand = load_predictand(os.path.join(predictand_path,'t_tide_'+str(temp_freq)+'h_hourly_deseasoned_predictands'),tg) #open predictand csv
-        elif predictand_source.lower() == 'codec':
-            predictand = load_codec_as_predictand(predictand_path,tg)
-        else:
-            raise Exception('Predictand source not recognized')
-            
-        predictand = predictand[(predictand['date']>=predictors.time.isel(time=0).values) & (predictand['date']<=predictors.time.isel(time=-1).values)]  # only use predictands when we also have predictor values
-        predictand = deseasonalize_df_var(predictand,'surge','date') #remove mean seasonal cycle
-
-        predictand = predictand.set_index('date').resample(str(temp_freq)+'h').fillna(method=None) #insert nans where timesteps are missing
-        predictand = predictand.reset_index()[['surge','date','lon','lat']]
-        #predictand['surge'] = predictand['surge'].rolling(window=int(12/temp_freq+1),min_periods=int(12/temp_freq+1),center=True).mean() #crude way to filter out peaks due to uncorrected tides (Tiggeloven et al., 2021)
-
-        ### (3) Configure sets of hyperparameters to run with
+        ### (2) Configure sets of hyperparameters to run with
         all_settings = list(itertools.product(*hyperparam_options))
         n_settings = len(all_settings)
 
@@ -109,7 +92,7 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
         else:
             selected_settings = all_settings
         
-        ### (4) Execute training & evaluation (n_iterations * n_runs times):
+        ### (3) Execute training & evaluation (n_iterations * n_runs times):
         for it in np.arange(n_iterations): #for each iteration
             tg_datasets = [] #list to store output
             
@@ -117,43 +100,28 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
                 
                 this_batch_size,this_n_steps,this_n_convlstm,this_n_convlstm_units,this_n_dense,this_n_dense_units,this_dropout,this_lr,this_l2,this_dl_alpha = these_settings #pick hyperparameters for this run
 
-                #generate train, validation and test splits
-                idx_train,idx_val,idx_test,x_train,x_val,x_test,y_train,y_val,y_test = split_predictand_and_predictors_with_stratified_years(predictand,predictors,split_fractions,this_n_steps,
-                                                                                                                                             strat_start_month,seed=strat_seed,how=strat_metric)
+                #generate train, validation and test splits & prepare input for model
+                model_input = preprocessing.Input(predictors,predictand,model_architecture)
+                model_input.split_stratified(split_fractions,this_n_steps,strat_start_month,strat_seed,strat_metric)
+                y_train_mean,y_train_sd = model_input.standardize()
+        
+                model_input.compute_denseloss_weights(this_dl_alpha) #generate the Denseloss weights for each split
+               
+                x_train,y_train,w_train = model_input.get_windowed_filtered_np_input('train',this_n_steps) #generate input for neural network model
+                x_val,y_val,w_val       = model_input.get_windowed_filtered_np_input('val',this_n_steps)
+                x_test,y_test,w_test    = model_input.get_windowed_filtered_np_input('test',this_n_steps)
 
-                #standardize the input based on the mean & sd of the train split
-                y_train,y_val,y_test,y_train_mean,y_train_sd = standardize_predictand_splits(y_train,y_val,y_test,output_transform = True)
-                x_train,x_val,x_test = standardize_predictor_splits(x_train,x_val,x_test)
-
-                if model_architecture == 'convlstm': #put the predictor data in the right input shape
-                    x_train, x_val, x_test = [stack_predictors_for_convlstm(k,predictor_vars) for k in [x_train,x_val,x_test]]
-                elif model_architecture == 'lstm':
-                    x_train, x_val, x_test = [stack_predictors_for_lstm(k,predictor_vars) for k in [x_train,x_val,x_test]]
-                else:
-                    raise Exception('Model architecture must be "lstm" or "convlstm".')
-
-                #generate the Denseloss weights
-                w_train,w_val = [get_denseloss_weights(k, alpha = this_dl_alpha ) for k in [y_train,y_val]] #generate DenseLoss weights for train & val splits   
-
-                #get values & timestamps of observations to compare predictions with
-                t_train = predictand['date'].values[idx_train][np.isfinite(y_train)]
-                t_val = predictand['date'].values[idx_val][np.isfinite(y_val)]
-                t_test = predictand['date'].values[idx_test][np.isfinite(y_test)]
-
-                #get windowed predictors & filter rows with nan observations from inputs (alternatively use tensorflow pipeline to avoid memory overflow; see end)
-                x_train,y_train,w_train = generate_windowed_filtered_np_input(x_train,y_train,this_n_steps,w_train)
-                x_val,y_val,w_val = generate_windowed_filtered_np_input(x_val,y_val,this_n_steps,w_val)
-                x_test,y_test = generate_windowed_filtered_np_input(x_test,y_test,this_n_steps)
-
-                o_train,o_val,o_test = [y_train_sd * k + y_train_mean for k in [y_train,y_val,y_test]] #back-transform observations
+                o_train,o_val,o_test = [model_input.y_train_sd * k + model_input.y_train_mean for k in [y_train,y_val,y_test]] #back-transform observations
 
                 #build model
                 if model_architecture == 'convlstm':
                     model = build_ConvLSTM2D_with_channels(this_n_convlstm, this_n_dense,(np.ones(this_n_convlstm)*this_n_convlstm_units).astype(int), 
-                                              (np.ones(this_n_dense)*this_n_dense_units).astype(int),this_n_steps,n_cells,n_cells,len(predictor_vars), 'convlstm0',this_dropout, this_lr, lf,l2=this_l2)
+                                              (np.ones(this_n_dense)*this_n_dense_units).astype(int),this_n_steps,
+                                                           len(predictors.data.lat_around_tg),len(predictors.data.lon_around_tg),len(predictor_vars), 1,'convlstm0',this_dropout, this_lr, lf,l2=this_l2)
                 elif model_architecture == 'lstm':
                     model = build_LSTM_stacked(this_n_convlstm, this_n_dense,(np.ones(this_n_convlstm)*this_n_convlstm_units).astype(int), 
-                                      (np.ones(this_n_dense)*this_n_dense_units).astype(int),this_n_steps,n_cells,n_cells,len(predictor_vars), 'lstm0',this_dropout, this_lr, lf,l2=this_l2)
+                                      (np.ones(this_n_dense)*this_n_dense_units).astype(int),(this_n_steps,
+                                                            len(predictors.data.lon_around_tg)**2 * len(predictor_vars)),1,'lstm0',this_dropout, this_lr, lf,l2=this_l2)
 
                 #train model:
                 if this_dl_alpha: #if using DenseLoss weights
@@ -164,14 +132,14 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
                                               callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience,restore_best_weights=True),GC_Callback()],verbose=2) #with numpy arrays input
 
                 #make predictions & back-transform
-                yhat_train = model.predict(x_train,verbose=0).flatten()*y_train_sd + y_train_mean
-                yhat_val = model.predict(x_val,verbose=0).flatten()*y_train_sd + y_train_mean
-                yhat_test = model.predict(x_test,verbose=0).flatten()*y_train_sd + y_train_mean
-
+                yhat_train = model.predict(x_train,verbose=0)*model_input.y_train_sd + model_input.y_train_mean#.flatten()*y_train_sd + y_train_mean
+                yhat_val = model.predict(x_val,verbose=0)*model_input.y_train_sd + model_input.y_train_mean#.flatten()*y_train_sd + y_train_mean
+                yhat_test = model.predict(x_test,verbose=0)*model_input.y_train_sd + model_input.y_train_mean#.flatten()*y_train_sd + y_train_mean
+                
                 #store results into xr dataset for current settings and iteration
-                ds_train = train_predict_output_to_ds(o_train,yhat_train,t_train,these_settings,model_architecture,lf_name)
-                ds_val = train_predict_output_to_ds(o_val,yhat_val,t_val,these_settings,model_architecture,lf_name)
-                ds_test = train_predict_output_to_ds(o_test,yhat_test,t_test,these_settings,model_architecture,lf_name)
+                ds_train = train_predict_output_to_ds(o_train,yhat_train,model_input.t_train,these_settings,np.array([tg]),model_architecture,lf_name)
+                ds_val = train_predict_output_to_ds(o_val,yhat_val,model_input.t_val,these_settings,np.array([tg]),model_architecture,lf_name)
+                ds_test = train_predict_output_to_ds(o_test,yhat_test,model_input.t_test,these_settings,np.array([tg]),model_architecture,lf_name)
           
                 ds_i = xr.concat((ds_train,ds_val,ds_test),dim='split',coords='different') #concatenate results for each split
                 ds_i = ds_i.assign_coords(split = ['train','val','test'])
@@ -202,9 +170,9 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
             out_ds = xr.concat(tg_datasets,dim='i',coords='different')
             out_ds = add_error_metrics_to_prediction_ds(out_ds,[.95,.98,.99,.995],3) #optional third argument 'max_numT_between_isolated_extremes' to exclude extremes isolated by more than n timesteps from another extreme from evaluation (to avoid including extremes mainly due to semi-diurnal tides, see manuscript for more explanation)
 
-            out_ds = out_ds.assign_coords(tg = np.array([tg])) #add TG information
-            out_ds = out_ds.assign_coords(lon = ('tg',np.array([predictand['lon'].values[0]])))
-            out_ds = out_ds.assign_coords(lat = ('tg',np.array([predictand['lat'].values[0]])))
+            #out_ds = out_ds.assign_coords(tg = np.array([tg])) #add TG information
+            out_ds = out_ds.assign_coords(lon = ('tg',np.array([predictand.data['lon'].values[0]])))
+            out_ds = out_ds.assign_coords(lat = ('tg',np.array([predictand.data['lat'].values[0]])))
 
             if len(n_steps) == 1: #if n_steps is constant across i, obs doesn't need to have i as a dimension. Saves storage.
                 out_ds['o'] = out_ds['o'].isel(i=0,drop=True)
@@ -220,7 +188,7 @@ def train_and_predict(model_architecture,loss_function,hyperparam_options,
             my_path = os.path.join(output_dir,'performance',model_architecture)
             my_fn = model_architecture+'_'+str(temp_freq)+'h_'+tg.replace('.csv','')+'_'+lf_name+'_hp1_ndeg'+str(predictor_degrees)+'_it'
             
-            out_ds.to_netcdf(os.path.join(my_path,my_fn+str(len(fnmatch.filter(os.listdir(my_path),my_fn+'*')))+'.nc'),mode='w')
+            #out_ds.to_netcdf(os.path.join(my_path,my_fn+str(len(fnmatch.filter(os.listdir(my_path),my_fn+'*')))+'.nc'),mode='w')
     return out_ds
 
 
@@ -255,18 +223,17 @@ if __name__ == "__main__":
     #configure standard settings if running main:
     
     #i/o
-    predictor_path  = 'gs://leap-persistent/timh37/era5_predictors/'
-    predictand_path = '/home/jovyan/test_surge_models/input/'
-    predictand_source = 'codec'
-    output_dir = '/home/jovyan/test_surge_models/results/nns_ndeg_test/' #'/home/jovyan/test_surge_models/results/nns/'
+    predictor_path  = 'gs://leap-persistent/timh37/era5_predictors/3hourly/'
+    predictand_path = '/home/jovyan/test_surge_models/input/CoDEC_ERA5_at_gesla3_tgs_eu_hourly_anoms.nc'#'/home/jovyan/test_surge_models/input/t_tide_3h_hourly_deseasoned_predictands'
+    output_dir = '/home/jovyan/test_surge_models/results/nns_codec_test/' #'/home/jovyan/test_surge_models/results/nns/'
     store_model = 0#1 #whether to store the tensorflow models
     temp_freq = 3 # [hours] temporal frequency to use
     
     #training
     predictor_vars = ['msl','u10','v10','w'] #variables to use
-    n_runs = 3 #how many hyperparameter combinations to run
+    n_runs = 1 #how many hyperparameter combinations to run
     n_iterations = 1 #how many iterations to run per hyperparameter combination
-    n_epochs = 150 #how many training epochs
+    n_epochs = 1 #how many training epochs
     patience = 13 #early stopping patience
     loss_function = {'mse':'mse'} # default tensorflow loss function string or string of custom loss function of surgeNN.losses (e.g., 'gevl({gamma})')
     
@@ -284,8 +251,8 @@ if __name__ == "__main__":
     n_convlstm_units = np.array([32]).astype('int')
     n_dense = np.array([2]).astype('int')
     n_dense_units = np.array([32]).astype('int')
-    dropout = np.array([0.2])#np.array([0.1,0.2])
-    lrs = np.array([5e-5])#np.array([1e-5,5e-5,1e-4])
+    dropout = np.array([0.1,0.2])
+    lrs = np.array([1e-5,5e-5,1e-4])
     l1s = np.array([0.02])
     
     hyperparam_options = [batch_size, n_steps, n_convlstm, n_convlstm_units,
@@ -293,7 +260,7 @@ if __name__ == "__main__":
     
     out_ds = train_and_predict(model_architecture,loss_function,hyperparam_options, #execute
                       split_fractions,strat_metric,strat_start_month,strat_seed,tgs,n_runs,n_iterations,n_epochs,patience,
-                     predictor_path,predictor_vars,predictor_degrees,predictand_source,predictand_path,temp_freq,output_dir,store_model)
+                     predictor_path,predictor_vars,predictor_degrees,predictand_path,temp_freq,output_dir,store_model)
     
 '''
 #Tensorflow pipeline for loading in batches:
