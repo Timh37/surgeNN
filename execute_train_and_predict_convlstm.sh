17ba7d3caa821030a343d5000c87db24e04e5308
Tim @ LEAP <a@b>
2025-09-30 09:05:00 +0000

    minor updates

--- execute_train_and_predict_convlstm.sh
+++ execute_train_and_predict_convlstm.sh
@@ -1,5 +1,5 @@
 #execute train_and_predict.py, either specifying none or 4 arguments like so: python train_and_predict.py "brest-822a-fra-uhslc.csv" "lstm" "np.array([0,1,3,5]).astype('int')" "5"
-#how to call: bash execute_train_and_predict.sh "brest-822a-fra-uhslc.csv" 
+#how to call: bash execute_train_and_predict_convlstm.sh 'alicante_i_outer_harbour-alio-esp-da_mm.csv'
 for i in $(seq 1 5);
 do
     echo $i
